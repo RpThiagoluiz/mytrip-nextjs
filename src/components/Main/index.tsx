31d52694de253f084ccfd8955fc989b25cf135d5
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-06-28 11:24:59 -0300

    init

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,21 +1,3 @@
-import * as S from './styles'
-//Assim, de olhar vc ja sabe q isso é um component de stylo.
-
-const Main = () => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um atamo e React Avancando escrito ao lado"
-    />
-    <S.Title>React Avancado</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </S.Description>
-    <S.Ilustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com codigo."
-    />
-  </S.Wrapper>
-)
+const Main = () => <h1>My trip</h1>
 
 export default Main
