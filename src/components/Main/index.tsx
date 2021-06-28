3e9f4dfc2e267a409a720ab92cef1278f9310c29
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-06-28 11:54:29 -0300

    feat: leaflet

--- src/components/Main/index.tsx
+++
@@ -1,3 +0,0 @@
-const Main = () => <h1>My trip</h1>
-
-export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
-import Main from 'components/Main'
+import dynamic from 'next/dynamic'
+
+//Export tem que ser default da pagina
+const Map = dynamic(() => import('components/Map'), { ssr: false })
+
+//O leaflet precisa do dinamic, para isso precisamos utilizar ele pela doc do nextjs/dynamic
 
 export default function Home() {
-  return <Main />
+  return <Map />
 }
